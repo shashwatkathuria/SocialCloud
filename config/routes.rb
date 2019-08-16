73456ea3c548a18fcd52fbd7b6f1e33ca7a730d1
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-08-16 23:39:22 +0530

    Changed root route and added other routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :users do
+    resources :posts
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  # get '/signup' => 'users#new'
+  # post '/users' => 'users#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
