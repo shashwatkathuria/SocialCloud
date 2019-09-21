d3d88fe529d25516436074ecec50b92cbff32ddb
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-09-21 12:20:09 +0530

    Dropped previous table and added a new one with devise gem.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190816231307) do
+ActiveRecord::Schema.define(version: 20190921033301) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
     t.string   "phone"
-    t.string   "email"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "password_digest"
-    t.string   "password_confirmation"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
