a8401a20513caabb705f27fc27a2e5ce02c1932f
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-09-24 14:18:58 +0530

    Removed some functions handled by devise and removed has_secure_password as it handles it itself.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+    skip_before_action :authenticate_user!, only: [:search]
     def index
         if current_user
           @user = current_user
@@ -7,24 +7,24 @@ class UsersController < ApplicationController
           redirect_to '/login'
         end
     end
-    def new
-        @user = User.new
-    end
-
-    def create
-        @user = User.new(validate_params)
-
-        if validate_password
-            # byebug
-            @user.save
-            redirect_to @user
-
-        else
-            @failureMessage = "Invalid credentials. Please try again."
-            redirect_to :action => "new", failureMessage: @failureMessage
-        end
-
-    end
+    # def new
+    #     @user = User.new
+    # end
+    #
+    # def create
+    #     @user = User.new(validate_params)
+    #
+    #     if validate_password_and_email
+    #         @user.save
+    #         @successMessage = "User Successfully Created. Please Login."
+    #         redirect_to controller:"sessions", action:"new", successMessage: @successMessage
+    #
+    #     else
+    #         @failureMessage = "Invalid credentials. Please try again."
+    #         redirect_to :action => "new", failureMessage: @failureMessage
+    #     end
+    #
+    # end
 
     def show
         @user = User.find(params[:id])
@@ -32,16 +32,17 @@ class UsersController < ApplicationController
 
     def search
         @users = User.where("name like ?", "%" + params[:searchQuery] + "%")
-        puts @users
     end
 
-    private
-      def validate_params
-          params.require(:user).permit(:name, :email, :password, :phone, :password_confirmation)
-      end
+    # private
+    #   def validate_params
+    #       params.require(:user).permit(:name, :email, :password, :phone, :password_confirmation)
+    #   end
+    #
+    #   def validate_password_and_email
+    #     @user.password == @user.password_confirmation && User.where(email:@user.email).count == 0
+    #   end
 
-      def validate_password
-        @user.password == @user.password_confirmation
-      end
+
 
 end
