00b410a6d253405585bf3c4847a4cb7bdb74563b
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-09-30 00:24:00 +0530

    New posts controller.

---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get posts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get posts_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get posts_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get posts_create_url
+    assert_response :success
+  end
+
+  test "should get search" do
+    get posts_search_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get posts_delete_url
+    assert_response :success
+  end
+
+end
