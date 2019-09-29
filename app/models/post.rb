46a24167c46585ead17bb6103e1dab0d7cdebb57
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-09-30 00:19:39 +0530

    Added post image and image caption attributes for post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Paperclip
   field :name, type: String
   field :content, type: String
   field :user_id, type: Integer
   field :time, type: Time, default: ->{ Time.now }
+  field :image_caption, type: String
+
+  has_mongoid_attached_file :post_image
+  validates_attachment_content_type :post_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
