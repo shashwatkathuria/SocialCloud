8c099de4ae528cca44987324a0ad6e2082d30d01
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-10-07 21:14:46 +0530

    Splitted full name into first and last name

---
+++ db/migrate/20191007014640_removing_name_field_from_users.rb
@@ -0,0 +1,7 @@
+class RemovingNameFieldFromUsers < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190925052450) do
+ActiveRecord::Schema.define(version: 20191007014640) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "phone"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
@@ -22,6 +21,8 @@ ActiveRecord::Schema.define(version: 20190925052450) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
