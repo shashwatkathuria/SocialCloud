85cfcc700741f73b6c9ad9435cb89994eb4dba2f
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-10-11 10:10:08 +0530

    Removed unrequired new, create and private helper functions, as they are handled by devise. Corrected search query according to the latest migration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,42 +7,13 @@ class UsersController < ApplicationController
           redirect_to '/login'
         end
     end
-    # def new
-    #     @user = User.new
-    # end
-    #
-    # def create
-    #     @user = User.new(validate_params)
-    #
-    #     if validate_password_and_email
-    #         @user.save
-    #         @successMessage = "User Successfully Created. Please Login."
-    #         redirect_to controller:"sessions", action:"new", successMessage: @successMessage
-    #
-    #     else
-    #         @failureMessage = "Invalid credentials. Please try again."
-    #         redirect_to :action => "new", failureMessage: @failureMessage
-    #     end
-    #
-    # end
 
     def show
         @user = User.find(params[:id])
     end
 
     def search
-        @users = User.where("name like ?", "%" + params[:searchQuery] + "%")
+        @users = User.where("first_name like ? or last_name like ?", "%" + params[:searchQuery] + "%", "%" + params[:searchQuery] + "%")
     end
-
-    # private
-    #   def validate_params
-    #       params.require(:user).permit(:name, :email, :password, :phone, :password_confirmation)
-    #   end
-    #
-    #   def validate_password_and_email
-    #     @user.password == @user.password_confirmation && User.where(email:@user.email).count == 0
-    #   end
-
-
 
 end
