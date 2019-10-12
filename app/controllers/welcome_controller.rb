eb54cdd67e3987ed6b9042157b8fc471fac87376
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-10-12 07:46:55 +0530

    Corrected sorting of posts.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    @posts = Post.all.order_by(time: :asc)
+    @posts = Post.all.order_by(time: :desc)
     @users = User.all
   end
 end
