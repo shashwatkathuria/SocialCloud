df761b7df7ab9e1a741ea9c40c353d937289861c
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-10-17 17:49:59 +0530

    Permitted user defined parameters for sign up and account update for necessary handling by devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+   protected
+
+   def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone])
+   end
+
 end
