1707ea6d6c5333acface5e0a2b5fbd0c509482c2
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-10-23 20:08:07 +0530

    Changed devise configuration to have both username or email as authentication options and defined the same as login attribute in user model. Also ensured uniqueness of email and username and permitted the same across sign up and account update. Defined regular expression for username and corner case for commonemail as username.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
    protected
 
    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :phone, :email])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :phone, :email])
    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,35 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+
+  validates_uniqueness_of :username
+  # validates_uniqueness_of :email => already taken care of by devise
+
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
 end
