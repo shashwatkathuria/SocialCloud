3359ee7fca8278c07570dbfe855e57c0cb1dd40f
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-10-26 12:09:05 +0530

    Added 3 random images if user is not signed in and all if signed in.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    @posts = Post.all.order_by(time: :desc)
+    if user_signed_in?
+        @posts = Post.all.order_by(time: :desc)
+    else
+        @posts = Post.all.sample(3)
+    end
     @users = User.all
   end
 end
