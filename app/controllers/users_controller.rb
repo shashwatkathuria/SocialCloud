6246ba3ba7b631490c573350afc3ca4914d838eb
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-10-31 00:05:49 +0530

    Implememted follow and unfollow action and modified index action, sending followers and following along too.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
     def index
         if current_user
           @user = current_user
+          @followers = []
+          for followerID in current_user.followers
+            @followers.push(User.find(followerID))
+          end
+          @following = []
+          for followingID in current_user.following
+            @following.push(User.find(followingID))
+          end
         else
           redirect_to '/login'
         end
@@ -10,6 +18,52 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+    end
+
+    def follow
+
+      @user = User.where(username: params[:username]).first
+      if @user.id  == current_user.id
+        redirect_to url_for action: "show_profile", controller: "users", username: @user.username
+      end
+      if @user.followers.include? current_user.id
+      else
+        @user.followers.push(current_user.id)
+        @user.save
+      end
+
+      if current_user.following.include? @user.id
+      else
+        current_user.following.push(@user.id)
+        current_user.save
+      end
+
+      redirect_to url_for action: "show_profile", controller: "users", username: @user.username
+    end
+
+    def unfollow
+
+      @user = User.where(username: params[:username]).first
+
+      if @user.id  == current_user.id
+        redirect_to url_for action: "show_profile", controller: "users", username: @user.username
+      end
+
+      if @user.followers.include? current_user.id
+        @user.followers.delete(current_user.id)
+        @user.save
+      else
+      end
+
+      if current_user.following.include? @user.id
+        current_user.following.delete(@user.id)
+        current_user.save
+      else
+
+      end
+
+      redirect_to url_for action: "show_profile", controller: "users", username: @user.username
+
     end
 
     def show_profile
