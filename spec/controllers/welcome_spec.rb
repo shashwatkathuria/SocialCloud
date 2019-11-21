b3a2ff7fbb7a75ee79693de2d9f113105c228415
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-11-21 11:10:38 +0530

    Added tests for welcome controller.

---
+++ spec/controllers/welcome_spec.rb
@@ -0,0 +1,20 @@
+require 'rails/mongoid'
+require 'spec_helper'
+require 'mongoid_paperclip'
+require 'paperclip/matchers'
+require 'rails_helper'
+require 'shoulda/matchers'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe 'GET #index' do
+    # render_views
+    # render_views -- To also include render html alongwith response
+    before { get :index }
+    # it { p response.body}
+    it { expect(response).to have_http_status(:success) }
+    it { should render_template('index') }
+    it { should respond_with 200 }
+    it { should_not use_before_action :authenticate_user! }
+    it { should render_with_layout('application') }
+  end
+end
