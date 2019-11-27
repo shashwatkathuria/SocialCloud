fa315c58d67e7c55050932a7ef7a0fc7b42fe5c3
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-11-27 18:25:29 +0530

    Added test spec for posts controller.

---
+++ spec/controllers/posts_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'mongoid_paperclip'
+require 'paperclip/matchers'
+require 'rails_helper'
+require 'shoulda/matchers'
+
+RSpec.describe PostsController, type: :controller do
+  describe 'GET #index' do
+    # render_views -- To also include render html alongwith response
+    before { get :index }
+    # it { expect(response).to have_http_status(:success) }
+    # it { should render_template('index') }
+    # it { should respond_with 200 }
+    # it { should_not use_before_action :authenticate_user! }
+    # it { should render_with_layout('application') }
+  end
+end
