d890328610959363252dc1ce31d732fcefe7a737
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-01-03 11:53:09 +0530

    Changed model to remove mongoid paperclip attachment and add base64 encoding as Heroku deletes system files after every once a while and aws-sdk wasn't so much necessary for now. Base64 for sql injection prevention, not plain binary encoding.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post
   include Mongoid::Document
-  include Mongoid::Paperclip
+
   field :user_id, type: Integer
   field :time, type: Time, default: ->{ Time.now }
   field :image_heading, type: String
   field :image_caption, type: String
+  field :image_content_type, type: String
+  field :image_base64, type: String
 
-  has_mongoid_attached_file :post_image
-  validates_attachment_content_type :post_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   def self.column_names
     self.attribute_names
   end
